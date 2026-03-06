37b90c936230b0d42d9d98415ed9493d1c01d4f0
Sebastián Villa <sebasviro567@gmail.com>
2026-03-06 16:34:17 +0000

    clase hoy

--- greedy.py
+++ greedy.py
@@ -54,7 +54,7 @@ atender_paciente()
 atender_paciente()
 
 """
-
+"""
 # Ejercicios de Greedy
 
 
@@ -80,3 +80,71 @@ resultado = seleccion_eventos(eventos)
 
 print("Eventos seleccionados:", resultado)
 print("Cantidad máxima:", len(resultado))
+
+"""
+"""
+# Clase 6/03/2026
+
+def mochila_fraccionaria(capacidad, objetos):
+    objetos_con_ratio = [(p, v, p/v) for p, v in objetos]
+    objetos_con_ratio.sort(key= lambda x: x[2], reverse=True)
+
+    valor_total = 0
+    objetos_almacenados = []
+
+    for peso, valor, ratio in objetos_con_ratio:
+        if capacidad >= peso:
+            valor_total += valor
+            capacidad -= peso
+            objetos_almacenados.append((peso, valor, 1))
+        elif capacidad > 0:
+            fraccion = capacidad / peso
+            valor_total += valor * fraccion
+            objetos_almacenados.append((peso, valor, fraccion))
+            break
+    
+    return objetos_almacenados, valor_total
+
+
+peso = [20, 40]
+valor = [10, 30]
+capacidad = 100
+
+objetos = list(zip(peso, valor))
+
+resultado, valor_total = mochila_fraccionaria(capacidad, objetos)
+
+print("Objetos almacenados:", resultado)
+print("Valor total:", valor_total)
+
+"""
+
+distancia_total = 50
+estaciones = [0, 15, 20, 40, 50]
+
+def minimo_paradas(distancia_total, capacidad, estaciones):
+    estaciones = [0] + sorted(estaciones) + [distancia_total]
+    paradas = []
+    combustible = capacidad 
+
+    for i in range(1, len(estaciones)):
+
+        distancia = estaciones[i] - estaciones[i-1]
+        if distancia > capacidad:
+            return -1
+        
+        if distancia > combustible:
+            paradas.append(estaciones[i-1])
+            combustible = capacidad
+
+        combustible -= distancia
+    
+    return paradas
+
+#Asignar tareas a servidores
+
+servidores = 3
+tareas = [10, 30, 20]
+
+import heapq
+
