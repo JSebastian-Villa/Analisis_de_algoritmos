116f36fa2287435fa388eaecc12064224243305f
Sebastián Villa <sebasviro567@gmail.com>
2026-04-15 16:25:57 +0000

    ....

--- dyv.py
+++ dyv.py
@@ -26,3 +26,41 @@ def potencia(x , n):
 print("=== Potencia Iterativa ===")
 res1 = potencia(2, 5)
 print(f"Resultado final: {res1}\n")
+
+
+#=====================================================================
+
+
+def max_min(arr):
+    maximo = minimo = arr[0]
+
+    for valor in arr[1:]:
+        if valor > maximo:
+            maximo = valor
+        if valor < minimo:
+            minimo = valor
+    return maximo, minimo
+
+#=========
+
+def max_min_dyv(arr, izq = 0, der = None):
+    if der is None:
+        der = len(arr) - 1
+    
+    if der - izq == 1:
+        if arr[izq] < arr[der]:
+            return arr[der], arr[izq]
+        return arr[izq], arr[der]
+    
+    if izq == der:
+        return arr[izq], arr[izq]
+    
+    medio = (izq + der) // 2
+
+    max_izq, min_izq = max_min_dyv(arr, izq, medio)
+    max_der, min_der = max_min_dyv(arr, medio + 1, der) 
+
+
+
+def ordenamiento():
+    
